e7f73caf050761d59cc6b8b6e5f766ad8b91d423
hirokr <hrr3645@gmail.com>
2025-10-27 22:15:33 +0600

    feat: implement Hero component with GSAP integration and initial structure

---
+++ src/components/Hero.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { useState, useRef } from 'react';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import { Divide, Menu } from 'lucide-react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { ScrollTrigger } from 'gsap/all';
+gsap.registerPlugin(ScrollTrigger);
+
+
+export default function Hero() {
+  
+  return (
+    <section>
+      
+    </section>
+  )
+}
